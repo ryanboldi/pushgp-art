bb8a666c9d05709ab180627e7b025a44616cd180
Ryan Boldi <ryan.boldi123@gmail.com>
2021-08-03 10:10:55 -0400

    Update core.clj

--- src/pushgp-art/core.clj
+++ src/pushgp-art/core.clj
@@ -1,6 +1,7 @@
 (ns pushgp-art.core
   (:require [quil.core :as q]
-            [quil.middleware :as m]))
+            [quil.middleware :as m]
+            [pushgp-art.utils :as utils]))
 
 (defn setup []
   ; Set frame rate to 30 frames per second.
@@ -15,12 +16,7 @@
 (defn draw [_]
   (q/background 255)
   ; create image and draw gradient on it
-  (let [im (q/create-image 256 256 :alpha)]
-    (dotimes [x 256]
-      (dotimes [y 256] (q/set-pixel im x y (q/color 256))))
-      (q/update-pixels im)
-    ; draw image twice
-      (q/image im 0 0)))
+  (q/image (utils/plushy->image utils/random-plushy) 0 0))
 
 
 (q/defsketch pushgp-art
