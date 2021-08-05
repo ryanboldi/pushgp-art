c0cf82b80924995485f8f2356ed8d6bc9574d33c
Ryan Boldi <ryan.boldi123@gmail.com>
2021-08-05 11:11:52 -0400

    using functional mode now

--- src/pushgp-art/core.clj
+++ src/pushgp-art/core.clj
@@ -5,27 +5,19 @@
 
 (defn setup []
   (q/frame-rate 1)
-  ;(let [plushies (repeatedly 10 utils/random-plushy)
-  ;      images (map #(utils/plushy->image % 50) plushies)]
-  ;  {:plushies plushies
-   ;  :images images}))
-)
+  (let [plushies (repeatedly 16 utils/random-plushy)
+        images (map #(utils/plushy->image % 50) plushies)]
+    {:plushies plushies
+     :images images}))
 
 (defn update [state] state)
 
 (defn draw [old-state]
   (q/background 255)
-  (let [plushies (repeatedly 4 utils/random-plushy)
-        images (map #(utils/plushy->image % 256) plushies)]
+  (let [images (:images old-state)]
    (dotimes [i (count images)]
-      (q/resize (nth images i) 256 256)
-      (q/image (nth images i) (* (mod i 2) 256) (* (quot i 2) 256))))
-  ;(let [images (:images old-state)]
-   ; (dotimes [i (count images)]
-  ;   (q/resize (nth i images) 100 100)
-     ; (q/image (nth i images) (* i 100))))
-  (q/save "assets/4-random-long-plushy.png")
-  (q/exit)
+    (q/resize (nth images i) 128 128)
+      (q/image (nth images i) (* (mod i 4) 128) (* (quot i 4) 128))))
   (q/no-loop))
 
 (q/defsketch pushgp-art
