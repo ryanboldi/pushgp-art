b2683537609035018e4b84222b1f2e3851eba505
Ryan Boldi <ryan.boldi123@gmail.com>
2021-08-04 21:51:53 -0400

    cleanup

--- src/pushgp-art/utils.clj
+++ src/pushgp-art/utils.clj
@@ -1,5 +1,6 @@
 (ns pushgp-art.utils
   (:require [propeller.genome :as genome]
+            [propeller.tools.math :as math]
             [propeller.push.interpreter :as interpreter]
             [propeller.push.state :as state]
             [propeller.push.instructions.numeric]
@@ -31,36 +32,19 @@
         0
         1))
 
-(repeatedly
-  population-size
-  #(hash-map :plushy (genome/make-random-plushy
-                      instructions
-                      max-initial-plushy-size)))
-
 (defn random-plushy [] (genome/make-random-plushy instructions max-initial-plushy-size))
-
-random-plushy
-
-(def push-program (genome/plushy->push random-plushy))
-
-push-program
-
-(state/peek-stack (interpreter/interpret-program
- push-program
- (assoc state/empty-state :input {:in1 256 :in2 256})
- 100) :float)
 
 (defn output->pixel-color [num]
   (->> num
        (* 2)
        (dec)
-       (#(if (< % 0) (* -1 %) %))
-       (float)
-       (* 255.0)))
+       (math/abs)
+       (* 255)
+       (math/ceil)))
 
-(output->pixel-color 1)
-
-(defn plushy->image [plushy width]
+(defn plushy->image 
+  "expresses the phenotype of a given plushy. Note: Can only be run from inside quil sketch functions"
+  [plushy width]
   (let [push-program (genome/plushy->push plushy)
         im (q/create-image width width :alpha)]
     (dotimes [x width]
@@ -71,7 +55,7 @@ push-program
                       (interpreter/interpret-program
                        push-program
                        (assoc state/empty-state :input {:in1 normalized-x :in2 normalized-y})
-                       width)
+                       100)
                       :float)]
           (q/set-pixel im x y (q/color (output->pixel-color output))))))
     im))
